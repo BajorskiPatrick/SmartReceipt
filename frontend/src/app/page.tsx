9c58e0748bcc72dff371c92b74528b4987e395cf
galekkapi <kapigalson@gmail.com>
2025-12-13 15:51:41 +0100

    poprawienie interceptora setupAxios.ts

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Dashboard from "./Dashboard";
+import "@/api-client/setupAxios";
 
 export default function DashboardPage() {
   const router = useRouter();
