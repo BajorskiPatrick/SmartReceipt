615f49b155eafdb775942b6ff1045187a0a6c6a8
BajorskiPatrick <bajorski16@gmail.com>
2025-12-22 00:47:45 +0100

    Feat: implement new yearly spending summary endpoint

--- backend/src/main/java/com/sp/smartreceipt/dashboard/controller/DashboardController.java
+++ backend/src/main/java/com/sp/smartreceipt/dashboard/controller/DashboardController.java
@@ -2,6 +2,7 @@ package com.sp.smartreceipt.dashboard.controller;
 
 import com.sp.smartreceipt.dashboard.service.DashboardService;
 import com.sp.smartreceipt.model.DashboardData;
+import com.sp.smartreceipt.model.YearlySpendingSummary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,5 +18,11 @@ public class DashboardController {
     @ResponseStatus(HttpStatus.OK)
     public DashboardData getDashboardData(@RequestParam Integer year, @RequestParam Integer month) {
         return dashboardService.getDashboardData(year, month);
+    }
+
+    @GetMapping("/yearly-spending-summary")
+    @ResponseStatus(HttpStatus.OK)
+    public YearlySpendingSummary getYearlySpendingSummary(@RequestParam Integer year) {
+        return dashboardService.getYearlySpendingSummary(year);
     }
 }
